ea93227f83f56f4e6a71701cf20ecc07d460c79f
Nikhil Raj <44263051+nikhilraj01@users.noreply.github.com>
2020-10-13 13:54:47 +0530

    Google and Facebook login added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React from "react";
-import Login from "./login.js";
-
-function App() {
-  return <Login />;
-}
-
-export default App;
+import { Route } from "react-router-dom";
+import Navigation from "./components/Navigation";
+import Signup from "./components/Signup";
+import Header from "./components/Header";
+import Login from "./components/Login";
+import { Account } from "./components/Accounts";
+export default () => {
+  return (
+    <div className="login-container">
+      <Header />
+      <Account>
+        <Route path="/signup" component={Signup} />
+        <Route path="/login" component={Login} />
+        <Route path="/" exact component={Navigation} />
+      </Account>
+    </div>
+  );
+};
+/* 
+<Signup />
+<br />
+<p>OR, if you already have an account</p>
+<Login /> 
+*/
